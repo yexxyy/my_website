f132394a5ff6f7d9f4a61373dc15bc8b9a08fd72
叶显勇 <xianyong.ye@monstar-lab-cn.com>
2016-09-28 14:34:54 +0800

    欢迎来到管理界面

--- mysite/management/views.py
+++ mysite/management/views.py
@@ -5,7 +5,9 @@ from django.template import loader
 from django.views.decorators.http import require_GET, require_POST
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-
+from django.contrib.auth import authenticate, login, logout
+from django.http import JsonResponse, HttpResponseBadRequest, HttpResponseNotFound, HttpResponse, HttpResponseForbidden
+from django.views.decorators.http import require_GET, require_POST
 # Create your views here.
 
 
@@ -17,13 +19,16 @@ def index(request):
 
 def user_login(request):
     if request.method=='GET':
-        print '哈哈哈'
         return render(request, 'management/login.html')
-
-
-
-
     else:
         username=request.POST.get('username')
         password=request.POST.get('password')
-    return HttpResponse('login page')
+        print username
+        user=authenticate(username=username,password=password)
+
+        if user is not None:
+            return render(request,'management/manage_page.html')
+        else:
+            return HttpResponseBadRequest('Login fail')
+
+        return HttpResponseNotFound("User does not exist")
--- mysite/record/static/record/record_list.js
+++ mysite/record/static/record/record_list.js
@@ -3,31 +3,13 @@ host='http://localhost:8001'
 
 //全局变量,用于保存发布时间
 var temp_publish_date
-var current_date
-var yestoday_date 
+
     //文档就绪函数
 $(document).ready(function(){
 
       get_records_content()
-      get_current_date()
+});
 
-})
-
-function get_current_date(){
-	current_date=get_date_string(0)
-	yestoday_date=get_date_string(-1)
-}
-
-function get_date_string(count){ 
-	var dd = new Date()
-	dd.setDate(dd.getDate()+count)//获取AddDayCount天后的日期 
-	var year = dd.getFullYear()
-	var month = dd.getMonth()+1;//获取当前月份的日期 
-	var day = dd.getDate()
-	result_date=year+'-'+(month<10?'0'+month:month)+'-'+(day<10?'0'+day:day)
-	console.log('获取的日期：'+result_date)
-	return result_date
-} 
 
 function get_records_content(){
 	$.ajax({
@@ -65,33 +47,18 @@ function create_type_video_html(object){
     var cover_url = host+object.banner;
 	var $elem = $($template.html())
 
-	var temp_Publish_label=$elem.find('.publish_time')
-	
-	if (temp_publish_date!=object.publish_date) {
-
-		switch (object.publish_date){
-			case current_date:{
-				temp_Publish_label.text('今天')
-				break
-			}
-			case yestoday_date:{
-				temp_Publish_label.text('昨天')
-				break
-			}
-			default :{
-				temp_Publish_label.text(object.publish_date)
-				break
-			}
-		}
-		temp_publish_date=object.publish_date
-		$elem.find('.devider').attr('width',0)
+	var temp_label=$elem.find('.publish_time')
+	if (temp_publish_date==object.publish_date) {
+		temp_label.addClass('height',)
 	}
+
+	$elem.find('.publish_time').text(object.publish_date)
+
+
 
 	$elem.find('.description').text(object.article_description)
     $elem.find('.banner').attr('src', cover_url)
     $elem.find('.video').text(object.video)
-
-
 	$container.append($elem) //在容器结尾放入元素
 }
 
@@ -106,29 +73,7 @@ function create_type_travel_program_html(object){
 
 	var $elem=$($template.html())
 
-	var temp_Publish_label=$elem.find('.publish_time')
-
-	if (temp_publish_date!=object.publish_date) {
-
-		switch (object.publish_date){
-			case current_date:{
-				temp_Publish_label.text('今天')
-				break
-			}
-			case yestoday_date:{
-				temp_Publish_label.text('昨天')
-				break
-			}
-			default :{
-				temp_Publish_label.text(object.publish_date)
-				break
-			}
-		}
-		temp_publish_date=object.publish_date
-		$elem.find('.devider').attr('width',0)
-		$elem.find('publish_time').attr('height',80)
-	}
-	
+	$elem.find('.publish_time').text(object.publish_date)
 	$elem.find('.title').text(object.title)
 	$elem.find('.description').text(object.article_description)
 
