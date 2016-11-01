a3c7597198cd1257d26b692a1752ea69a2d4cd96
叶显勇 <xianyong.ye@monstar-lab-cn.com>
2016-11-01 17:51:35 +0800

    提交结果处理未完成

--- mysite/record/views.py
+++ mysite/record/views.py
@@ -80,6 +80,6 @@ def store_user_commit_data(request):
     except:
         return HttpResponseBadRequest("提交失败,请重试")
 
-    return HttpResponse("提交成功")
+    return render(request,'record/commit_result.html',name)
 
 
