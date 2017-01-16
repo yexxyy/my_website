803e33fb3912d53dfecb099e045d236063644c87
YE <xianyong.ye@monstar-lab-cn.com>
2017-01-16 17:33:23 +0800

    爱来咯额度hosts

--- mysite/mysite/settings/server.py
+++ mysite/mysite/settings/server.py
@@ -3,6 +3,8 @@ from base import *
 
 
 DEBUG=False
+#You must set settings.ALLOWED_HOSTS if DEBUG is False.
+ALLOWED_HOSTS = ['*']
 
 #when diploy nginx server , we need collect all django's static files
 STATIC_ROOT = '/root/my_website_server/static'
