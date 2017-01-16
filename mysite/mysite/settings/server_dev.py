9f35795f5042e44d11d0f2297a0728098587dfb8
YE <xianyong.ye@monstar-lab-cn.com>
2017-01-16 16:30:51 +0800

    重命名

---
+++ mysite/mysite/settings/server_dev.py
@@ -0,0 +1,10 @@
+from base import *
+
+
+
+DEBUG=True
+
+MEDIA_ROOT='/root/my_website_server/upload_assets'
+
+IMAGE_CACHE_ROOT='/root/my_website_server/upload_assets'
+
--- mysite/mysite/settings/settings_server_dev.py
+++
@@ -1,9 +0,0 @@
-from settings_base import *
-
-
-
-DEBUG=True
-
-MEDIA_ROOT='/root/my_website_server/upload_assets'
-
-IMAGE_CACHE_ROOT='/root/my_website_server/upload_assets'
